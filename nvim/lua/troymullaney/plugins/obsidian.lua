1de7cef950136a57b35011466b776b83606227e3
Troy Mullaney <troy@clearvoice.com>
2024-01-16 15:18:00 -0700

    Aerospace and obsidian changes

--- nvim/lua/troymullaney/plugins/obsidian.lua
+++ nvim/lua/troymullaney/plugins/obsidian.lua
@@ -2,6 +2,7 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	dependencies = { "nvim-lua/plenary.nvim" },
+	notes_subdir = "notes",
 	opts = {
 		workspaces = {
 			{
