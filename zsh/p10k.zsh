8af6d1d9035e47ce0617565040adcb709b08cbce
Troy Mullaney <troy@clearvoice.com>
2024-01-16 15:33:24 -0700

    Update minimum branch length for prompt

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -393,10 +393,10 @@
 
     if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
       local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-      # If local branch name is at most 32 characters long, show it in full.
-      # Otherwise show the first 12 … the last 12.
+      # If local branch name is at most 40 characters long, show it in full.
+      # Otherwise show the first 16 … the last 16.
       # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
+      (( $#branch > 40 )) && branch[17,-17]="…"  # <-- this line
       res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
     fi
 
